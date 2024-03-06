20960191b74d6fdf724e975d40820f58d8480bee
Yurj <yurj9999@yandex.ru>
2024-03-06 17:25:56 +0300

    add tasks

---
+++ Task 1/Program.cs
@@ -0,0 +1,50 @@
+﻿class Checker
+{
+    public static int[] GetNumbersFromInterval(int from, int to)
+    {
+        List<int> result = new List<int>();
+
+        for (int i = from; i < to + 1; i++) {
+            result.Add(i);
+        }
+
+        return result.ToArray();
+    }
+
+    public static int[] CheckNumbersForNatural(int[] numbers, int counter, List<int> preparedData)
+    {
+        if (counter == numbers.Length) {
+            return preparedData.ToArray();
+        }
+
+        if (numbers[counter] > 0) {
+            preparedData.Add(numbers[counter]);
+        }
+
+        counter++;
+        
+        return CheckNumbersForNatural(numbers, counter, preparedData);
+    }
+
+    public static void ShowNaturalNumbers(int[] numbers)
+    {
+        List<int> preparedData = new List<int>();
+        int counter = 0;
+
+        int[] result = CheckNumbersForNatural(numbers, counter, preparedData);
+
+        Console.WriteLine(string.Join(", ", result));
+    }
+}
+
+class NaturalNumber
+{
+    public static void Main()
+    {
+        int M = 3;
+        int N = 5;
+
+        int[] data = Checker.GetNumbersFromInterval(M, N);
+        Checker.ShowNaturalNumbers(data);
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,25 @@
+﻿class Checker
+{
+    public static int Akkerman(int m, int n) {
+        if (m == 0) {
+            return n + 1;
+        } else if ((m != 0) && (n == 0)) {
+            return Akkerman(m - 1, 1);
+        } else {
+            return Akkerman(m - 1, Akkerman(m, n - 1));
+        }
+    }
+}
+
+class AkkermanMaker
+{
+    public static void Main()
+    {
+        int M = 2;
+        int N = 3;
+
+        int result = Checker.Akkerman(M, N);
+
+        Console.WriteLine(result);
+    }
+}
